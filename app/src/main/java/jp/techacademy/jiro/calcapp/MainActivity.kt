2df04d2c1a1c54498e70778274300abb7d070269
Jiro0512 <nube200501@gmail.com>
2024-01-20 11:36:25 +0900

    課題４（UI）再提出(20240120)

--- app/src/main/java/jp/techacademy/jiro/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/jiro/calcapp/MainActivity.kt
@@ -3,11 +3,7 @@ package jp.techacademy.jiro.calcapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
-import androidx.appcompat.app.AlertDialog
-=======
 import android.widget.Toast
->>>>>>> fea793a (課題４（UI）再提出)
 import jp.techacademy.jiro.calcapp.databinding.ActivityMainBinding
 
 
@@ -22,49 +18,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         //editText1とeditText２に数字を入力のうえ、ボタン１（足し算ボタン）を押したら、足し算用の関数を呼び出す。
-<<<<<<< HEAD
-        //数字未入力の場合はアラートを出す設定にしたがったが上手くアラートが出ず。
-        binding.button1.setOnClickListener{
-            val input1 = binding.editText1.text.toString().toFloat()
-            val input2 = binding.editText2.text.toString().toFloat()
-            if(input1 !=null && input2 !=null) {
-                calculation_plus(input1, input2)
-            } else{
-                showAlertDialog()
-            }
-        }
-        //editText1とeditText２に数字を入力のうえ、ボタン２（引き算ボタン）を押したら、引き算用の関数を呼び出す。
-        //数字未入力の場合はアラートを出す設定にしたがったが上手くアラートが出ず。
-        binding.button2.setOnClickListener{
-            val input1 = binding.editText1.text.toString().toFloat()
-            val input2 = binding.editText2.text.toString().toFloat()
-            if(input1 !=null && input2 !=null) {
-                calculation_minus(input1, input2)
-            } else{
-                showAlertDialog()
-            }
-        }
-        //editText1とeditText２に数字を入力のうえ、ボタン３（掛け算ボタン）を押したら、掛け算用の関数を呼び出す
-        //数字未入力の場合はアラートを出す設定にしたがったが上手くアラートが出ず。
-        binding.button3.setOnClickListener{
-            val input1 = binding.editText1.text.toString().toFloat()
-            val input2 = binding.editText2.text.toString().toFloat()
-            if(input1 !=null && input2 !=null) {
-                calculation_multiple(input1, input2)
-            } else{
-                showAlertDialog()
-            }
-        }
-        //editText1とeditText２に数字を入力のうえ、ボタン４（割り算ボタン）を押したら、割り算用の関数を呼び出す
-        //数字未入力の場合はアラートを出す設定にしたがったが上手くアラートが出ず。
-        binding.button4.setOnClickListener{
-            val input1 = binding.editText1.text.toString().toFloat()
-            val input2 = binding.editText2.text.toString().toFloat()
-            if(input1 !=null && input2 !=null) {
-                calculation_division(input1, input2)
-            } else{
-                showAlertDialog()
-=======
         //数字未入力の場合はToastでアラートを出す、数字入力がある場合は計算へ
         binding.button1.setOnClickListener{
             val input1 = binding.editText1.text.toString()
@@ -110,7 +63,6 @@ class MainActivity : AppCompatActivity() {
                 val number1 = input1.toFloat()
                 val number2 = input2.toFloat()
                 calculation_division(number1, number2)
->>>>>>> fea793a (課題４（UI）再提出)
             }
         }
     }
@@ -138,15 +90,4 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("VALUE", "${factor1 / factor2}".toFloat())
         startActivity(intent)
     }
-<<<<<<< HEAD
-    //数字が未入力の場合にアラートを出すための関数
-    private fun showAlertDialog(){
-        val alertDialogBuilder = AlertDialog.Builder(this)
-        alertDialogBuilder.setTitle("注意")
-        alertDialogBuilder.setMessage("入力漏れがあります")
-        val alertDialog = alertDialogBuilder.create()
-        alertDialog.show()
-    }
-=======
->>>>>>> fea793a (課題４（UI）再提出)
 }
